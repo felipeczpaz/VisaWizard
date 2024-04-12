e5c72f5c6bc2bb7ef23743b26d426da978ff5231
felipeczpaz <felipecezarpazzz@proton.me>
2024-04-11 22:09:22 -0300

    :tada: Project files

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # VisaWizard
-VisaWizard is your automated companion for breezing through the US Visa application process.
+
+VisaWizard is a TamperMonkey script designed to simplify the US Visa application process by automating the selection of checkboxes for specific sections of the application form. By automatically checking checkboxes based on predefined criteria, VisaWizard streamlines the application process, saving time and ensuring accuracy. Ideal for frequent applicants, VisaWizard enhances efficiency and convenience, empowering users to submit well-prepared applications with ease.
+
+## Usage
+
+1. **Installation**:
+
+   - Install the TamperMonkey browser extension for your browser (available for Chrome, Firefox, Safari, and other browsers).
+   - Create a new script in TamperMonkey.
+   - Copy the script provided in the `VisaWizard.js` file into the TamperMonkey script editor.
+
+2. **Running the Script**:
+   - Once the script is installed and configured, it will run by clicking the "Fill Form" button added to the top-right corner of the page.
+
+## Note
+
+- This script is intended for use with the US Visa application form. Ensure that you review the filled sections after running the script to verify the accuracy of the information.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please feel free to open an issue or create a pull request on the GitHub repository.
+
+## License
+
+This script is licensed under the [MIT License](LICENSE).
---
+++ VisaWizard.js
@@ -0,0 +1,87 @@
+// ==UserScript==
+// @name         VisaWizard
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Simplify your US Visa application process with automated checkbox selection, saving time and ensuring accuracy.
+// @author       Felipe Cezar Paz (https://github.com/felipeczpaz)
+// @match        https://ceac.state.gov/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  function runScript() {
+    const elementsToCheck = [
+      "rblDisease_1",
+      "rblDisorder_1",
+      "rblDruguser_1",
+      "rblArrested_1",
+      "rblControlledSubstances_1",
+      "rblProstitution_1",
+      "rblMoneyLaundering_1",
+      "rblHumanTrafficking_1",
+      "rblAssistedSevereTrafficking_1",
+      "rblHumanTraffickingRelated_1",
+      "rblIllegalActivity_1",
+      "rblTerroristActivity_1",
+      "rblTerroristSupport_1",
+      "rblTerroristOrg_1",
+      "rblTerroristRel_1",
+      "rblGenocide_1",
+      "rblTorture_1",
+      "rblExViolence_1",
+      "rblChildSoldier_1",
+      "rblReligiousFreedom_1",
+      "rblPopulationControls_1",
+      "rblTransplant_1",
+      "rblImmigrationFraud_1",
+      "rblDeport_1",
+      "rblRemovalHearing_1",
+      "rblFailToAttend_1",
+      "rblVisaViolation_1",
+      "rblChildCustody_1",
+      "rblVotingViolation_1",
+      "rblRenounceExp_1",
+      "rblAttWoReimb_1",
+    ];
+
+    elementsToCheck.forEach((elementId) => {
+      const element = document.getElementById(
+        `ctl00_SiteContentPlaceHolder_FormView1_${elementId}`
+      );
+      if (element) {
+        element.checked = true;
+      }
+    });
+
+    const updateButton = document.getElementById(
+      "ctl00_SiteContentPlaceHolder_UpdateButton3"
+    );
+    if (updateButton) {
+      updateButton.click();
+    }
+  }
+
+  // Create and append button to run the script
+  function addButton() {
+    const button = document.createElement("button");
+    button.textContent = "Fill Form";
+    button.style.position = "fixed";
+    button.style.top = "20px";
+    button.style.right = "20px";
+    button.style.padding = "10px 20px";
+    button.style.border = "none";
+    button.style.borderRadius = "5px";
+    button.style.backgroundColor = "#007bff";
+    button.style.color = "#fff";
+    button.style.fontFamily = "Arial, sans-serif";
+    button.style.fontSize = "16px";
+    button.style.cursor = "pointer";
+    button.addEventListener("click", runScript);
+    document.body.appendChild(button);
+  }
+
+  // Call the addButton function to add the button when the page loads
+  addButton();
+})();
